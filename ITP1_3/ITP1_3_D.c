84de76832a59d24b8ad5ddd07b9e55cc5b8871ff
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2018-05-11 20:37:16 +0900

    Add presolution for 1-3d

---
+++ ITP1_3/ITP1_3_D.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+  int a, b, c;
+  int i;
+  int count = 0;
+  
+  scanf("%d %d %d", &a, &b, &c);
+
+  for(i = a; i <= b; i++){
+    if(c % i == 0) count++;
+  }
+
+  printf("%d\n", count);
+  
+  return 0;
+}
